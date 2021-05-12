e545b9ee23db4db5784d4defd146be18dcbdb9b4
Juan David Correa <sye.jdcorrea@gmail.com>
2021-05-11 19:05:59 -0500

    full calcultions without rounding to 2 decimals and without validate division 0

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 
 let numberOnScreen = 0;
 let numberOnMemory = 0;
-let executeOperation = false;
+let prevKeyisNan = true;
+let equalKeyActive = false;
 let resultValue = 0;
 let stringNumberbyUser = '';
 let operator = '';
@@ -15,71 +16,89 @@ screenResult.textContent = 0;
 //Clic listener for keys and assignation of temp values
 document.querySelectorAll(".button-key").forEach(item => {
   item.addEventListener('click', event => {
-    actionByUser(item);
-    if (executeOperation) {
+    const buttonPressed = item.innerHTML;
+    //debugger
+    actionByUser(buttonPressed);
+    if (equalKeyActive && prevKeyisNan) {
       switch (operator) {
         case 'plus':
           numberOnMemory = sumNumbers(numberOnMemory, numberOnScreen);
+          break;
         case 'minus':
           numberOnMemory = substractNumbers(numberOnMemory, numberOnScreen);
+          break;
         case 'multiply':
           numberOnMemory = multiplyNumbers(numberOnMemory, numberOnScreen);
+          break;
         case 'divide':
           numberOnMemory = divideNumbers(numberOnMemory, numberOnScreen);
-      }
-      screenResult.textContent = numberOnMemory; 
+          break;
+        }
+      screenResult.textContent = numberOnMemory;
     }
   })
 });
 
 //Functions
-function actionByUser(item) {
-  const buttonPressed = item.innerHTML;
-  console.log(buttonPressed);
-
+function actionByUser(buttonPressed) {
+  
   if (isNaN(buttonPressed)) {
-    executeOperation = false;
-    screenOperator.textContent = buttonPressed;
-    
-    switch (buttonPressed) {
-      case '+':
-        operator = 'plus';
-        break;
-      case '−':
-        operator = 'minus';
-        break;
-      case '×':
-        operator = 'multiply';
-        break;
-      case '÷':
-        operator = 'divide';
-        break;
-      case '=':
-        executeOperation = true;
-        break;
-      case 'CE':
-        cleanVariables();
-    }
-
-    if (numberOnMemory !== 0) {
-      numberOnScreen = parseFloat(stringNumberbyUser);
-    } else {
-      numberOnMemory = parseFloat(stringNumberbyUser);
-    }
-    stringNumberbyUser = '';
+    prevKeyisNan = true;
+    equalKeyActive = false;
+    checkOperation(buttonPressed);
   }
   else {
+    prevKeyisNan = false;
+    equalKeyActive = false;
     screenOperator.textContent = '';
     stringNumberbyUser += buttonPressed;
     screenResult.textContent = stringNumberbyUser;
   }
 }
 
+function checkOperation(buttonPressed) {
+  screenOperator.textContent = buttonPressed;
+
+  switch (buttonPressed) {
+    case '+':
+      operator = 'plus';
+      break;
+    case '−':
+      operator = 'minus';
+      break;
+    case '×':
+      //debugger;
+      operator = 'multiply';
+      break;
+    case '÷':
+      operator = 'divide';
+      break;
+    case '=':
+      equalKeyActive = true;
+      break;
+    case 'CE':
+      cleanVariables();
+  }
+  if (stringNumberbyUser != '') {
+    if (numberOnMemory !== 0) {
+      numberOnScreen = parseFloat(stringNumberbyUser);
+    } else {
+      numberOnMemory = parseFloat(stringNumberbyUser);
+    }
+  }
+  stringNumberbyUser = '';
+}
+
 function cleanVariables() {
   numberOnScreen = 0;
   numberOnMemory = 0;
+  stringNumberbyUser = 0;
   screenOperator.textContent = '';
   screenResult.textContent = '0';
+}
+
+function executeCalculatorFunction(operator) {
+
 }
 
 function sumNumbers(number1, number2) {
